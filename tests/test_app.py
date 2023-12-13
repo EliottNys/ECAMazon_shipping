4627fbe59d76c996a3e1d15f647f507db30a091d
Eliott Nys <eliott.nys@gmail.com>
2023-12-13 19:12:06 +0100

    corrected code to work with integer IDs instead of strings

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,13 +20,13 @@ class FlaskTestCase(unittest.TestCase):
     def test_new_parcel(self, mock_send_to_dispatching, mock_get_user_address):
         mongo.db = self.mock_db
         response = self.app.post('/new_parcel', data=json.dumps({
-            'order_id': '123',
-            'user_id': '456',
-            'parcel_id': '789'
+            'order_id': 123,
+            'user_id': 456,
+            'parcel_id': 789
         }), content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        mock_get_user_address.assert_called_once_with('456')
-        mock_send_to_dispatching.assert_called_once_with('789', 'Some Address')
+        mock_get_user_address.assert_called_once_with(456)
+        mock_send_to_dispatching.assert_called_once_with(789, 'Some Address')
 
     @mongomock.patch(servers=(('localhost', 27017),))
     def test_all_parcels(self):
@@ -37,7 +37,7 @@ class FlaskTestCase(unittest.TestCase):
     @mongomock.patch(servers=(('localhost', 27017),))
     def test_get_parcel_info(self):
         mongo.db = self.mock_db
-        mongo.db.parcels.insert_one({'parcel_id': '123', 'order_id': '123', 'user_id': '456', 'address': 'Some Address', 'status': 'Processing'})
+        mongo.db.parcels.insert_one({'parcel_id': 123, 'order_id': 123, 'user_id': 456, 'address': 'Some Address', 'status': 'Processing'})
         response = self.app.get('/parcel/123')
         self.assertEqual(response.status_code, 200)
 
@@ -45,8 +45,8 @@ class FlaskTestCase(unittest.TestCase):
     def test_new_parcel_missing_data(self):
         mongo.db = self.mock_db
         response = self.app.post('/new_parcel', data=json.dumps({
-            'order_id': '123',
-            'user_id': '456'
+            'order_id': 123,
+            'user_id': 456
         }), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
